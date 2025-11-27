d2a7d7b7563f17fb9f8a292831c163d8983d54dd
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-11-27 16:05:07 +0300

    fix install

--- build_package.sh
+++ build_package.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 VERSION=$1
 DIR_FILES=$2
+DOVECOT_DIRECTORY=$3
 
 sudo apt update
 sudo apt-get install -y zip unzip
 
+sudo cp -r $DOVECOT_DIRECTORY/Pack/src $DIR_FILES/Pack/src
 cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
 sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
