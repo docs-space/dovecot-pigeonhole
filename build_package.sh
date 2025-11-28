13f2f6fa8e17b8f2d3667c7bee4926a4df093245
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-11-28 16:15:04 +0300

    add sudo

--- build_package.sh
+++ build_package.sh
@@ -8,11 +8,11 @@ sudo apt-get install -y zip unzip
 
 sudo cp -r $DOVECOT_DIRECTORY/Pack/src $DIR_FILES/Pack/src
 
-sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/src/DEBIAN/control
-sed -i "s/Source:.*/Source: r7mdaserver (${VERSION})/" $DIR_FILES/Pack/src/DEBIAN/control
-sed -i "s/Package:.*/Package: r7mdaserver/" $DIR_FILES/Pack/src/DEBIAN/control
+sudo sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/src/DEBIAN/control
+sudo sed -i "s/Source:.*/Source: r7mdaserver (${VERSION})/" $DIR_FILES/Pack/src/DEBIAN/control
+sudo sed -i "s/Package:.*/Package: r7mdaserver/" $DIR_FILES/Pack/src/DEBIAN/control
 
-dpkg-deb -b $DIR_FILES/Pack/src $DIR_FILES/Packages/r7mailserver_${VERSION}.deb
+sudo dpkg-deb -b $DIR_FILES/Pack/src $DIR_FILES/Packages/r7mailserver_${VERSION}.deb
 
 #cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
 #sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
