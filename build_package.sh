8d6cf84586afd26e509cf4aacea51dad6b646993
Ilya Talipov <talipov.ilya@r7-office.ru>
2026-04-06 13:55:30 +0300

    change build script

--- build_package.sh
+++ build_package.sh
@@ -11,6 +11,8 @@ sudo apt-get install -y zip unzip
 sudo sed -i "s/Version:.*/Version: ${VERSION}/" $DOVECOT_DIRECTORY/Pack/src/DEBIAN/control
 sudo sed -i "s/Source:.*/Source: r7mdaserver (${VERSION})/" $DOVECOT_DIRECTORY/Pack/src/DEBIAN/control
 sudo sed -i "s/Package:.*/Package: r7mdaserver/" $DOVECOT_DIRECTORY/Pack/src/DEBIAN/control
+sudo sed -i "s/Installed-Size:.*/Installed-Size: $(du -sk $DOVECOT_DIRECTORY/Pack/src | cut -f1)/" $DOVECOT_DIRECTORY/Pack/src/DEBIAN/control
+( cd "$DOVECOT_DIRECTORY/Pack/src" && rm -f DEBIAN/md5sums && find . -type f ! -path '*/DEBIAN/*' -exec md5sum {} > DEBIAN/md5sums \; )
 
 sudo dpkg-deb -b $DOVECOT_DIRECTORY/Pack/src $DIR_FILES/Packages/r7mdaserver_${VERSION}.deb
 
